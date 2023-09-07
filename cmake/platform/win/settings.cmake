1db1dbeef00db22bfa9ec8afef62fd5d32ef72a0
geektcp <geektcp@gmail.com>
2023-09-07 12:05:57 +0800

    compile suggest

--- cmake/platform/win/settings.cmake
+++ cmake/platform/win/settings.cmake
@@ -1,3 +1,6 @@
+# It is not recommended to compile on the Windows platform
+# we suggest you compile it on the Linux platform, for example CentOS 6.5, or CentOS 7.x
+
 if ( MSVC )
   include("${ROOT_DIR}/cmake/compiler/msvc/settings.cmake")
 endif()
